64ee0448e9d6e0491410749797747be61492c78c
Ben Miles <bcgm3studios@gmail.com>
2023-08-10 18:09:45 -0400

    Header MVP

--- src/App.vue
+++ src/App.vue
@@ -13,61 +13,15 @@ export default {
 </script>
 
 <template>
-
-	<!-- <RouterLink to="/xyz" class="module module-items"></RouterLink> -->
-
-	<!-- <header>
-		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-		<div class="wrapper">
-		<HelloWorld msg="You did it!" />
+	<div id="pda">
+		<div id="screen">
+			<TheHeader />
+			<RouterView :key="$route.path" />
+			<!-- <TheFooter /> -->
 		</div>
-  	</header> -->
-
-	<!-- <main>
-		<TheWelcome />
-	</main> -->
-
-  <div id="pda">
-	<div id="screen">
-
-		
-	</div><!-- /#screen -->
-
-	<TheHeader />
-	<RouterView :key="$route.path" />
-	<!-- <TheFooter /> -->
-
-</div><!-- /#pda -->
-<h1 id="title"><span>SUB</span>NAUTICA PDA</h1>
-<div id="bkg"></div>
+	</div>
+	<h1 id="title"><span>SUB</span>NAUTICA PDA</h1>
+	<div id="bkg"></div>
 </template>
 
-<style scoped>
-/* header {
-  line-height: 1.5;
-}
-`
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
-</style>
+<style></style>
---
+++ src/components/icons/IconDocument.vue
@@ -0,0 +1,10 @@
+<template>
+	<svg viewBox="0 0 36 34">
+		<linearGradient id="gradient" gradientUnits="userSpaceOnUse" x1="18" y1="1.6665" x2="18" y2="31.6665">
+			<stop offset="0" style="stop-color:#F3F9FD"/>
+			<stop offset="1" style="stop-color:#79D1F6"/>
+		</linearGradient>
+		<path d="M24.817,1.667H1v30h34V11.85L24.817,1.667z M6,6.667h13v2H6V6.667z M6,11.667h13v2H6V11.667z
+		M31,23.667H6v-2h25V23.667z M31,18.667H6v-2h25V18.667z M24,12.667v-7l7,7H24z"/>
+	</svg>
+</template>
