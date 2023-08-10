be54aefc9b654426fc2da0975a568eca3ff19bd7
Ben Miles <bcgm3studios@gmail.com>
2023-08-10 18:17:55 -0400

    Notes MVP

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,32 +1,26 @@
 <script>
 import { RouterLink } from 'vue-router'
-// import IconTranslator from '../components/icons/IconTranslator.vue';
-// import IconItems from '../components/icons/IconItems.vue'
 
 export default {
-	components: { 
-		// IconTranslator,
-		// IconItems 
-	}
+	data() {
+		return {
+			notes: ""
+		}
+	},
 }
 </script>
 
 <template>
-
 	<!-- HEADING -->
 	<div id="heading">
 		<h2>Notes</h2>
 	</div>
-
 	<!-- PANES -->
 	<div id="panes">
-
 		<div class="pane" id="notes-pane">
-			<!-- <textarea v-model="notes" placeholder="Keep your notes here."></textarea> -->
+			<textarea v-model="notes" placeholder="Keep your notes here."></textarea>
 		</div>
-
 	</div>
-
 </template>
 
 <style>
