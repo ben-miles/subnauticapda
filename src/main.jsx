3077b8605e0330754f4e6504cca360f9013c5037
Ben Miles <bcgm3studios@gmail.com>
2024-09-04 23:54:42 -0400

    Move Components and Layout from main.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<div id="pda">
-			<div id="screen">
-				<Header />
-				<App />
-				{/* <RouterView :key="$route.path" @toggleItem="toggleItem" :itemsData="itemsData" /> */}
-			</div>
-		</div>
-		<Footer />
-		<div id="bkg"></div>
+		<App />
 	</StrictMode>,
 )
