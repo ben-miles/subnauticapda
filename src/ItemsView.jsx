71d74875998206c620f22e43b21e1d7ad154aebe
Ben Miles <bcgm3studios@gmail.com>
2024-09-05 01:03:36 -0400

    Import code from ItemsView.vue

---
+++ src/ItemsView.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+// export default {
+// 	components: {},
+// 	data() {
+// 		return {}
+// 	},
+// 	emits: ["toggleItem"],
+// 	methods: {
+// 		// Toggle group
+// 		toggle: function(groupId){
+// 			let group = document.getElementById(groupId);
+// 			group.classList.toggle("collapsed");
+// 		},
+// 		toggleItem(itemId) {
+// 			// console.log(itemId)
+// 			this.$emit("toggleItem", itemId);
+// 		}
+// 	},
+// 	props: {
+// 		itemsData: Object,
+// 		list: Array
+// 	}
+// }
+
+function ItemsView() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			{/* <div className="pane" id="items-pane">
+				<div id="items-container">
+					<div v-for="(group, groupindex) in itemsData" className="group collapsed" :id="group.id">
+						<h3 v-on:click="toggle(group.id)">{{ group.name}}</h3>
+						<!-- <div v-for="(item, itemindex) in group.items" className="item" v-on:click="addToList(groupindex, itemindex)"> -->
+						<div v-for="(item, itemindex) in group.items" className="item" v-on:click="toggleItem(item.id)">
+							<img :src="`src/assets/images/${item.id}.png`" />
+							<span>{{ item.name}}</span>
+						</div>
+					</div>
+				</div>
+			</div> */}
+		</>
+	)
+}
+
+export default ItemsView;
