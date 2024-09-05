9db3965362b292c61fb8515e2af64b823448a65a
Ben Miles <bcgm3studios@gmail.com>
2024-09-04 23:38:50 -0400

    Restore Dynamic CurrentYear in Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -18,13 +18,14 @@ export default {
 </script> */}
 
 function Footer() {
+	const currentYear = new Date().getFullYear();
 	return (
 		<>
 			<footer>
-				<div class="title">
+				<div className="title">
 					<h1 id="title"><span>SUB</span>NAUTICA PDA</h1>
 				</div>
-				<div class="copyright">© 1234 - 5678 <a href="https://benmiles.com/" target="_blank">Ben Miles</a>. <a href="#" target="_self">Subnautica PDA</a> is not affiliated with <a href="http://subnauticagame.com/" target="_blank">Subnautica</a> or <a href="https://unknownworlds.com/" target="_blank">Unknown Worlds</a>. See <a href="https://github.com/ben-miles/subnauticapda" target="_blank">the IconGitHub GitHub repo</a> for more information!</div>
+				<div className="copyright">© 2020 - {currentYear} <a href="https://benmiles.com/" target="_blank">Ben Miles</a>. <a href="#" target="_self">Subnautica PDA</a> is not affiliated with <a href="http://subnauticagame.com/" target="_blank">Subnautica</a> or <a href="https://unknownworlds.com/" target="_blank">Unknown Worlds</a>. See <a href="https://github.com/ben-miles/subnauticapda" target="_blank">the IconGitHub GitHub repo</a> for more information!</div>
 			</footer>
 		</>
 	)
