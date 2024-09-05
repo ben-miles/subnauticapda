ace020ffd6358ce872c26f9ac2ee1a7574fe51e2
Ben Miles <bcgm3studios@gmail.com>
2024-09-05 11:21:49 -0400

    Header Code Cleanup
    Replaced anchors with buttons, fixed erroneously duplicated id/aria-label/title attributes, removed old Vue code, moved export to the same line as the function declaration.

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,39 +3,14 @@ import IconBook from './IconBook.jsx';
 import IconDocument from './IconDocument.jsx';
 import IconWrench from './IconWrench.jsx';
 
-function Header() {
+export default function Header() {
 	return (
 		<>
 			<div id="tabs">
-				<a href="#" className='tab' id="list" aria-label="List" title="List"><IconBook/><div>List</div></a>
-				<a href="#" className='tab' id="list" aria-label="List" title="List"><IconWrench/><div>Items</div></a>
-				<a href="#" className='tab' id="list" aria-label="List" title="List"><IconDocument/><div>Notes</div></a>
-			{/* <RouterLink to="/list" class="tab" id="list" aria-label="List" title="List">
-				<IconBook />
-			</RouterLink>
-			<RouterLink to="/items" class="tab" id="items" aria-label="Items" title="Items">
-				<IconWrench />
-			</RouterLink>
-			<RouterLink to="/notes" class="tab" id="notes" aria-label="Notes" title="Notes">
-				<IconDocument />
-			</RouterLink> */}
+				<button href="#" className='tab' id="list" aria-label="List" title="List"><IconBook/><span>List</span></button>
+				<button href="#" className='tab' id="items" aria-label="Items" title="Items"><IconWrench/><span>Items</span></button>
+				<button href="#" className='tab' id="notes" aria-label="Notes" title="Notes"><IconDocument/><span>Notes</span></button>
 			</div>
 		</>
 	)
 }
-
-export default Header;
-
-
-// import { RouterLink } from 'vue-router'
-// import IconBook from './icons/IconBook.vue'
-// import IconWrench from '../components/icons/IconWrench.vue'
-// import IconDocument from '../components/icons/IconDocument.vue';
-
-// export default {
-// 	components: { 
-// 		IconBook,
-// 		IconWrench,
-// 		IconDocument
-// 	}
-// }
