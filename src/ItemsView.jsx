fbe55bffe1ab9c5a25e7e08e405082bf51c455eb
Ben Miles <bcgm3studios@gmail.com>
2024-09-06 16:22:31 -0400

    Recreate Groups and Items Display, toggleGroup()

--- src/ItemsView.jsx
+++ src/ItemsView.jsx
@@ -25,21 +25,30 @@ import { useState } from 'react';
 
 	const [count, setCount] = useState(0);
 export default function ItemsView({items, setItems}) {
+	const toggleGroup = (id) => {
+		let group = document.getElementById(id);
+		group.classList.toggle("collapsed");
+	};
 
 	return (
 		<>
-			{/* <div className="pane" id="items-pane">
-				<div id="items-container">
-					<div v-for="(group, groupindex) in itemsData" className="group collapsed" :id="group.id">
-						<h3 v-on:click="toggle(group.id)">{{ group.name}}</h3>
-						<!-- <div v-for="(item, itemindex) in group.items" className="item" v-on:click="addToList(groupindex, itemindex)"> -->
-						<div v-for="(item, itemindex) in group.items" className="item" v-on:click="toggleItem(item.id)">
-							<img :src="`src/assets/images/${item.id}.png`" />
-							<span>{{ item.name}}</span>
-						</div>
+		<div className="pane" id="items-pane">
+			<div id="items-container">
+				{items.map((group) => (
+					<div className='group collapsed' id={group.id} key={group.id}>
+						<h3 onClick={() => { toggleGroup(group.id); }}>{group.name}</h3>
+						{group.items.map((item) => (
+							<div className={'item ' + item.id} key={item.id}>
+								<img src={'./src/assets/images/' + item.id + '.png'} />
+								<span>{item.name}</span>
+							</div>
+						))}
 					</div>
 				</div>
 			</div> */}
 		</>
+				))}
+			</div>
+		</div>
 	)
 }
