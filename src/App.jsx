0687f9ef0dbc7eca3f02576e74d7a89c1ff2f876
Ben Miles <bcgm3studios@gmail.com>
2024-09-06 16:19:52 -0400

    Revert "Ungrouping" of Items Data
    At some point I removed all the "groups" from the items data, making them a sort-of flat file of all items lumped into one array... But I think that introduces some new issues regarding potential spoilers that I'm not sure yet how to work around, so I'm going back to the old data structure that keeps items in discrete groups.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react';
+import ItemsData from "./assets/data/items.json";
 import Navigation from './Navigation.jsx';
 import Title from './Title.jsx';
 import View from './View.jsx';
 import Footer from './Footer.jsx';
 
 export default function App() {
+	const [items, setItems] = useState(ItemsData);
 	const [view, setView] = useState('Home');
 	const [notes, setNotes] = useState('');
 	
