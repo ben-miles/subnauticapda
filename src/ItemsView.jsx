4df29a2cb0c70019f639353b32e885d72245d8a5
Ben Miles <bcgm3studios@gmail.com>
2024-09-08 15:25:28 -0400

    Add ToggleItems()
    This takes care of a feature that should have been in the original version, but I just couldn't work it out back then. Now any item that is clicked (and therefore added to the List view) visibly changes to indicate it's presence on the other screen.

--- src/ItemsView.jsx
+++ src/ItemsView.jsx
@@ -3,15 +3,25 @@ export default function ItemsView({view, items, setItems}) {
 		let group = document.getElementById(id);
 		group.classList.toggle("collapsed");
 	};
+	// toggleItem adds or removes the item from the list state object
+	const toggleItem = (groupIndex,itemIndex) => {
+		let itemsCopy = [...items];
+		itemsCopy[groupIndex].items[itemIndex].isActive = !itemsCopy[groupIndex].items[itemIndex].isActive;
+		setItems(itemsCopy);
+		// console.log(groupIndex, itemIndex, items[groupIndex].items[itemIndex]);
+	};
+
+
+
 	if (view === 'Items') {
 		return (
 			<div className="pane" id="items-pane">
 				<div id="items-container">
-					{items.map((group) => (
-						<div className='group collapsed' id={group.id} key={group.id}>
+					{items.map((group, groupIndex) => (
+						<div className='group collapsed' id={group.id} key={groupIndex}>
 							<h3 onClick={() => { toggleGroup(group.id); }}>{group.name}</h3>
-							{group.items.map((item) => (
-								<div className={'item ' + item.id} key={item.id}>
+							{group.items.map((item, itemIndex) => (
+								<div className={'item ' + (item.isActive ? 'active' : '')} id={item.id} key={itemIndex} onClick={() => { toggleItem(groupIndex,itemIndex); }}>
 									<img src={'./src/assets/images/' + item.id + '.png'} />
 									<span>{item.name}</span>
 								</div>
