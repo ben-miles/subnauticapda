869ad7f108ea4ec26e615ab4cb7031ae5ee8eaae
Ben Miles <bcgm3studios@gmail.com>
2024-09-10 13:26:20 -0400

    Create IconLocation.jsx

---
+++ src/IconLocation.jsx
@@ -0,0 +1,12 @@
+export default function IconLocation() {
+	return (
+		<svg viewBox="0 0 36 34">
+			<linearGradient id="gradient" gradientUnits="userSpaceOnUse" x1="18" y1="1" x2="18" y2="33">
+				<stop offset="0" stopColor="#F3F9FD"/>
+				<stop offset="1" stopColor="#79D1F6"/>
+			</linearGradient>
+			<polygon points="22.431,1 19.969,3.584 21.692,5.308 8.646,15.277 7.169,13.799 4.585,16.386 10.985,22.785 
+				0.77,33 5.815,33 13.569,25.246 19.969,31.646 22.431,29.185 21.079,27.707 30.924,14.661 32.646,16.386 35.23,13.799 	"/>
+		</svg>
+	)
+}
