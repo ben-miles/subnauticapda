4d6f66ecef624c86b5cf6d4f5827b92284e5b3fc
Ben Miles <bcgm3studios@gmail.com>
2024-09-24 10:57:40 -0400

    Add Item Filtering by Text Input
    MVP for this new feature, allows the user to filter items in the Items view by typing into a text input.

--- src/ViewItems.jsx
+++ src/ViewItems.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import IconPin from './IconPin.jsx';
 
 export default function ViewItems({view, groups, setGroups, items, setItems}) {
+	const [search, setSearch] = useState('');
 	const toggleGroup = (groupIndex) => {
 		let groupsCopy = {...groups};
 		groupsCopy[groupIndex].isOpen = !groupsCopy[groupIndex].isOpen;
@@ -20,14 +21,22 @@ export default function ViewItems({view, groups, setGroups, items, setItems}) {
 		setItems(itemsCopy);
 		localStorage.setItem('items', JSON.stringify(items));
 	};
+	const handleSearchChange = (e) => { 
+		const searchTerm = e.target.value;
+		setSearch(searchTerm);
+	}
 
 	if (view.filter(view => view.id === 'Items')[0].isActive) {
 		return (
 			<div className="pane items">
+				<input type="text" placeholder="Search..." value={search} onChange={handleSearchChange} />
+
 				{Object.entries(groups).map(([groupIndex, group]) => (
+					(Object.entries(items).filter(([itemIndex, item]) => item.group === group.name && item.name.toLowerCase().includes(search.toLowerCase())).length > 0) && (
 						<div className={'group ' + (group.isOpen ? 'open' : '')} key={group.id}>
 						<h3 onClick={() => { toggleGroup(groupIndex); }}>{group.name}</h3>
 
+						{Object.entries(items).filter(([itemIndex, item]) => item.group === group.name && item.name.toLowerCase().includes(search.toLowerCase())).map(([itemIndex, item]) => (
 							<div className={'item ' + (item.isPinned ? 'active' : '')} id={itemIndex} key={item.id} onClick={() => { toggleItem(item.id); }}>
 								{item.isPinned && <IconPin />}
 								<img src={'images/' + item.id + '.png'} alt={item.name} loading="lazy" />
